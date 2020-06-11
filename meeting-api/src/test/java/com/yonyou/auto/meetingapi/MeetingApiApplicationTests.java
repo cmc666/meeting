0a8ce27c5719d8206afe878678dcdb92cb5515ad
cuimc <615931914@qq.com>
2020-06-12 00:39:14 +0800

    初始化项目影院框架的服务模块和API模块

---
+++ meeting-api/src/test/java/com/yonyou/auto/meetingapi/MeetingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yonyou.auto.meetingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
