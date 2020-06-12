8c6c5be9367e7ce64f430464c19b139380651bcf
cuimc <615931914@qq.com>
2020-06-12 10:42:09 +0800

    新增电影目录结构修改pom的依赖关系

---
+++ meeting-service/README.md
@@ -0,0 +1,9 @@
+**meeting-service**
+
+
+    1.介绍:
+        meetins-service下是所有的基于dubbo作为服务模块,根据服务的不同创建不同的项目
+        
+    2.模块介绍:
+         meeting-service-demo: 样例服务模块
+         meeting-service-user: 用户服务模块
---
+++ meeting-service/meeting-service-demo/src/main/java/com/yonyou/meeting/service/demo/MeetingServiceDemoApplication.java
@@ -0,0 +1,17 @@
+package com.yonyou.meeting.service.demo;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cuimc
+ * @since 2020-06-12
+ */
+@SpringBootApplication
+@EnableDubboConfig
+public class MeetingServiceDemoApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
